54375be302a2d8bce3d7c31d32c0169e65dc1a99
Rob Rossmiller <rrossmiller3@gmail.com>
2023-08-25 22:39:17 -0400

    day 6 done

--- 2022/rust/src/day6.rs
+++ 2022/rust/src/day6.rs
@@ -1,16 +1,19 @@
 use std::{collections::HashSet, fs};
 
 pub fn run() {
-    pt1();
+    //pt1
+    decode(4);
+    //pt2
+    decode(14);
 }
 
-fn pt1() {
+fn decode(n: usize) {
     let data_path = "data/day6.txt";
     let data = fs::read_to_string(data_path).expect(format!("{data_path} not found").as_str());
 
     let chars: Vec<char> = data.chars().collect();
-    for i in 4..chars.len() {
-        if is_different(&chars[i - 4..i]) {
+    for i in n..chars.len() {
+        if is_different(&chars[i - n..i]) {
             println!("{}", i);
             break;
         }
